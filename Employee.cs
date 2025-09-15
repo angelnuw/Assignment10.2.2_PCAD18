35c981f0bf2712ddf25d4bad7d9ba752afc84265
Angel Nuw <angelnuw7@gmail.com>
2025-09-15 10:59:45 -0400

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment10._2._2
+{
+    public class Employee
+    {
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+        public double Salary { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assignment10._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { Name = "Alice", Age = 25, Salary = 6000 },
+                new Employee { Name = "Bob", Age = 32, Salary = 7000 },
+                new Employee { Name = "Charlie", Age =28, Salary = 4500 },
+                new Employee { Name = "David", Age = 27, Salary = 5200 },
+                new Employee { Name = "Eve", Age = 29, Salary = 8000 }
+            };
+
+            var filteredEmployees = from emp in employees
+                                    where emp.Salary > 5000 && emp.Age < 30
+                                    select emp;
+
+            Console.WriteLine("Employees with Salary > $5000 and Age < 30:");
+            foreach (var emp in filteredEmployees)
+            {
+                Console.WriteLine($"Name: {emp.Name}, Age: {emp.Age}, Salary: {emp.Salary}");
+            }
+        }
+    }
+}
